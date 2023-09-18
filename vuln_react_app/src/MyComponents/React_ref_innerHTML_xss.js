d5141de366e286f7623f4511b765822d0c888a23
Josh Grossman (Bounce Security) <97975715+joshbouncesecurity@users.noreply.github.com>
2023-09-18 17:05:13 +0300

    Vulnerability fix (powered by Mobb Autofixer) (#2)
    * mobb fix commit: f611c7ee-de77-4b64-8c30-00ddaae73bde
    
    * mobb fix commit: 1352b2f1-27ed-4b96-9031-ac105f90364d
    
    ---------
    
    Co-authored-by: Mobb autofixer <git@mobb.ai>

--- vuln_react_app/src/MyComponents/React_ref_innerHTML_xss.js
+++ vuln_react_app/src/MyComponents/React_ref_innerHTML_xss.js
@@ -32,9 +32,9 @@ export class React_ref_innerHTML_xss extends Component {
         });
         const response = await request.json();
         this.nameRef.current.innerHTML = response.name;
-        this.emailRef.current.innerHTML = response.email;
+        this.emailRef.current.innerText = response.email;
         this.websiteRef.current.setAttribute('href', response.website);
-        this.websiteRef.current.innerHTML = response.website;
+        this.websiteRef.current.innerText = response.website;
         document.getElementById('update').setAttribute('hidden', true);
         document.getElementById('updated').removeAttribute('hidden');
     }
